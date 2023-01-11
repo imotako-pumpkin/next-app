1cb0905a90664e6e12ef1811a7a885d4d5294f84
imotako-pumpkin <imotako_pumpkin@outlook.com>
2023-01-11 21:12:42 +0900

    背景追加

--- src/components/layout/PageCard.tsx
+++ src/components/layout/PageCard.tsx
@@ -1,26 +1,25 @@
-import { FC, ReactNode } from 'react'
-import { Card, CardBody, CardHeader, Heading } from '@chakra-ui/react'
-import { motion } from 'framer-motion'
+import { Card, CardBody, CardHeader, Heading } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import { FC, ReactNode } from "react";
 
-export type PageCardProps = { children: ReactNode; title: string }
+export type PageCardProps = { title: string; children: ReactNode };
 
-export const PageCard: FC<PageCardProps> = ({ children, title }) => {
+export const PageCard: FC<PageCardProps> = ({ title, children }) => {
   return (
     <>
       <motion.div
         initial={{ opacity: 0, x: 1000 }}
         animate={{ opacity: 1, x: 0 }}
         exit={{ opacity: 0 }}
-        transition={{ ease: 'easeOut', duration: 0.5 }}
+        transition={{ duration: 0.5, ease: "easeOut" }}
       >
-        <Card backgroundColor="gray.50" mt={10} mr={5} mb={10} ml={200}>
+        <Card backgroundColor="whiteAlpha.500" mt={10} mr={5} mb={10} ml={200}>
           <CardHeader>
             <Heading size="md">{title}</Heading>
           </CardHeader>
-
           <CardBody>{children}</CardBody>
         </Card>
       </motion.div>
     </>
-  )
-}
+  );
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,37 @@
 import "../styles/globals.css";
 
 import { Button, ChakraProvider, extendTheme, Stack } from "@chakra-ui/react";
+import { Cloud } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 import { AnimatePresence } from "framer-motion";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <>
+      <div className="fixed -z-10 h-screen w-full bg-zinc-700">
+        <Canvas
+          gl={{ alpha: true, antialias: false, depth: false, stencil: false }}
+          camera={{ far: 100, fov: 32.5, near: 1, position: [0, 0, 20] }}
+        >
+          <ambientLight />
+          <Cloud
+            position={[-4, -2, 0]}
+            args={[3, 2]}
+            speed={3}
+            color={"yellow"}
+          />
+          <Cloud position={[-4, 2, 0]} args={[3, 2]} speed={3} color={"blue"} />
+          <Cloud args={[3, 2]} speed={3} color={"green"} />
+          <Cloud position={[4, -2, 0]} args={[3, 2]} speed={3} color={"red"} />
+          <Cloud
+            position={[4, 2, 0]}
+            args={[3, 2]}
+            speed={3}
+            color={"purple"}
+          />
+        </Canvas>
+      </div>
       <ChakraProvider theme={theme}>
         <Stack spacing={4} direction="row" align="center">
           <Button colorScheme="teal" size="sm" onClick={() => router.push("/")}>
