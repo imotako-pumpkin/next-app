b6ffe49d445365faf378c25364e23f7d7497f267
imotako-pumpkin <imotako_pumpkin@outlook.com>
2023-01-18 21:29:29 +0900

    scene

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,67 +2,54 @@ import "../styles/globals.css";
 
 import { Button, ChakraProvider, extendTheme, Stack } from "@chakra-ui/react";
 import { Cloud } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
 import { AnimatePresence } from "framer-motion";
 import type { AppProps } from "next/app";
+
+import { Scene } from "@/components/three/Scene";
 
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <>
-      <div className="fixed -z-10 h-screen w-full bg-zinc-700">
-        <Canvas
-          gl={{ alpha: true, antialias: false, depth: false, stencil: false }}
-          camera={{ far: 100, fov: 32.5, near: 1, position: [0, 0, 20] }}
-        >
-          <ambientLight />
-          <Cloud
-            position={[-4, -2, 0]}
-            args={[3, 2]}
-            speed={3}
-            color={"yellow"}
-          />
-          <Cloud position={[-4, 2, 0]} args={[3, 2]} speed={3} color={"blue"} />
-          <Cloud args={[3, 2]} speed={3} color={"green"} />
-          <Cloud position={[4, -2, 0]} args={[3, 2]} speed={3} color={"red"} />
-          <Cloud
-            position={[4, 2, 0]}
-            args={[3, 2]}
-            speed={3}
-            color={"purple"}
-          />
-        </Canvas>
+      <Scene>
+        <Cloud position={[4, -2, 0]} args={[3, 2]} speed={1} color={"red"} />
+      </Scene>
+      <div className="absolute top-0 left-0 h-full w-full">
+        <ChakraProvider theme={theme}>
+          <Stack spacing={4} direction="row" align="center">
+            <Button
+              colorScheme="teal"
+              size="sm"
+              onClick={() => router.push("/")}
+            >
+              top
+            </Button>
+            <Button
+              colorScheme="teal"
+              size="sm"
+              onClick={() => router.push("/information")}
+            >
+              information
+            </Button>
+            <Button
+              colorScheme="teal"
+              size="sm"
+              onClick={() => router.push("/about")}
+            >
+              about
+            </Button>
+          </Stack>
+          {router.pathname === "/" ? (
+            <Component {...pageProps} />
+          ) : (
+            <AnimatePresence
+              mode="wait"
+              onExitComplete={() => window.scrollTo(0, 0)}
+            >
+              <Component key={router.asPath} {...pageProps} />
+            </AnimatePresence>
+          )}
+        </ChakraProvider>
       </div>
-      <ChakraProvider theme={theme}>
-        <Stack spacing={4} direction="row" align="center">
-          <Button colorScheme="teal" size="sm" onClick={() => router.push("/")}>
-            top
-          </Button>
-          <Button
-            colorScheme="teal"
-            size="sm"
-            onClick={() => router.push("/information")}
-          >
-            information
-          </Button>
-          <Button
-            colorScheme="teal"
-            size="sm"
-            onClick={() => router.push("/about")}
-          >
-            about
-          </Button>
-        </Stack>
-        {router.pathname === "/" ? (
-          <Component {...pageProps} />
-        ) : (
-          <AnimatePresence
-            mode="wait"
-            onExitComplete={() => window.scrollTo(0, 0)}
-          >
-            <Component key={router.asPath} {...pageProps} />
-          </AnimatePresence>
-        )}
-      </ChakraProvider>
     </>
   );
 }
