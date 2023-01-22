6121827f4e90d493e5962aefc614f416c21cc3b5
imotako-pumpkin <imotako_pumpkin@outlook.com>
2023-01-23 00:00:37 +0900

    pages

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,7 +2,9 @@ export default function About() {
   return (
     <>
       <p className=" text-2xl text-sky-100">ABOUT</p>
-      <p className=" whitespace-pre-wrap break-words">about</p>
+      <p className=" whitespace-pre-wrap break-words">
+        aboutaboutaboutaboutaboutaboutaboutaboutaboutaboutaboutaboutaboutaboutaboutaboutaboutaboutaboutaboutaboutabout
+      </p>
     </>
   );
 }
