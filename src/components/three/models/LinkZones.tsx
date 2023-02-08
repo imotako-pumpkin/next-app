95e190df0c4038aadf0b0222e95b734a0349015b
imotako-pumpkin <imotako_pumpkin@outlook.com>
2023-02-08 22:15:15 +0900

    linkzone

--- src/components/three/models/LinkZones.tsx
+++ src/components/three/models/LinkZones.tsx
@@ -1,7 +1,8 @@
-import { Center, Text3D } from "@react-three/drei";
+import { Box, Center, Float, Text3D } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
+import router from "next/router";
 import { FC, useRef, useState } from "react";
-import { Mesh, Vector3 } from "three";
+import { Material, Mesh, Vector3 } from "three";
 import helvetiker_bold from "three/examples/fonts/helvetiker_bold.typeface.json";
 
 import { useTopFieldContext } from "@/contexts/TopFieldContext";
@@ -46,38 +47,51 @@ const LinkZone: FC<LinkZoneProps> = (props) => {
 
   return (
     <>
-      <mesh
+      <Box
         name={name}
         ref={ref}
+        args={[5, 5, 0]} // Args for the buffer geometry
         rotation={[-0.5 * Math.PI, 0, 0]}
         receiveShadow
         position={position}
         onClick={() => {
           if (ref.current) {
             const { x, z } = ref.current.position;
-            dispatch({
-              payload: new Vector3(x, 0.5, z),
-              type: "UPDATE_HUMAN_COORDINATE",
-            });
+            const { x: _x, z: _z } = state.humanCoordinate;
+            if (x === _x && z === _z) {
+              router.push(`./${name}`);
+            } else {
+              dispatch({
+                payload: new Vector3(x, 0.5, z),
+                type: "UPDATE_HUMAN_COORDINATE",
+              });
+            }
           }
         }}
+        material-color={hovered ? "yellow" : color}
+        material={{ opacity: 1 } as Material}
+      />
+
+      <Float
+        speed={5}
+        rotationIntensity={0.5}
+        floatIntensity={1}
+        floatingRange={[-0.1, -0.1]}
       >
-        <planeGeometry args={[5, 5]} />
-        <meshStandardMaterial color={hovered ? "yellow" : color} />
-      </mesh>
-      <Center position={position} top>
-        <Text3D
-          font={helvetiker_bold as unknown as string}
-          rotation={[
-            hovered ? 0 : -0.5 * Math.PI,
-            hovered ? (1 / 6) * Math.PI : 0,
-            hovered ? 0 : (1 / 6) * Math.PI,
-          ]}
-        >
-          {name}
-          <meshNormalMaterial />
-        </Text3D>
-      </Center>
+        <Center position={position} top>
+          <Text3D
+            font={helvetiker_bold as unknown as string}
+            rotation={[
+              hovered ? 0 : -0.5 * Math.PI,
+              hovered ? (1 / 6) * Math.PI : 0,
+              hovered ? 0 : (1 / 6) * Math.PI,
+            ]}
+          >
+            {name}
+            <meshNormalMaterial />
+          </Text3D>
+        </Center>
+      </Float>
     </>
   );
 };
